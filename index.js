6772fd066d3a4e65e9b2775ba129648be021d69f
Akhia11 <100513117+Akhia11@users.noreply.github.com>
2022-05-25 19:53:31 +0300

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-
-client.once('ready', () => {
-	console.log('Ready!');
-});
-
-client.login('OTM2MjU0NDczMDQxNDg5OTIw.GuCLDb._CYYZUCeqTmnyFWI4rPQ9js4_ZVg6qbGN20SYs');
